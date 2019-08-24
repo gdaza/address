634edd4525126a5073d1385b0dfa3148edde4845
camii_160999@hotmail.com <camii_160999@hotmail.com>
2019-08-24 11:18:52 -0400

    solucion final

---
+++ address/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace address.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<address.Models.book> books { get; set; }
+    }
+}
