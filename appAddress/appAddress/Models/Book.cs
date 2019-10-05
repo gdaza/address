9bade284e163f1aa72579f17a0d3e92a32461af4
Gustavo Daza <gustavodaza2013@gmail.com>
2019-10-05 10:17:21 -0400

    cpm apiservice y models

---
+++ appAddress/appAddress/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿
+namespace appAddress.Models
+{
+    public enum TypeContact
+    {
+        telefono,
+        correo,
+        facebook,
+        twitter,
+        instagram,
+    }
+    public class book
+    {
+        
+        public int BookID { get; set; }
+        public string Name { get; set; }
+        public TypeContact MyProperty { get; set; } 
+        public string Contact { get; set; }
+    }
+}
