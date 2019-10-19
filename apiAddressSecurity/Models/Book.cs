fd184048bd72ed6062c033c38485b268b71c4008
tahynaritafay@gmail.com <tahynaritafay@gmail.com>
2019-10-19 10:25:07 -0400

    Token

---
+++ apiAddressSecurity/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿namespace apiAddressSecurity.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public enum TypeContact
+    {
+        telefono,
+        correo,
+        facebook,
+        twitter,
+        instagram,
+    }
+    public class Book
+    {
+        [Key]
+        public int BookID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public TypeContact MyProperty { get; set; }
+        [Required]
+        public string Contact { get; set; }
+    }
+}
